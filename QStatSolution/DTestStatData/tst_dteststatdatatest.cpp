33aa673352fa46f2d3d0dcb6f73a663ae7f1ee98
Boubacar Diarra <boubadiarra@gmail.com>
2016-05-02 00:23:22 +0200

    added some tests

--- QStatSolution/DTestStatData/tst_dteststatdatatest.cpp
+++ QStatSolution/DTestStatData/tst_dteststatdatatest.cpp
@@ -1,9 +1,14 @@
+#include <QDebug>
 #include <QString>
 #include <QtTest>
 #include <QCoreApplication>
 #include <QMap>
 /////////////////////////////////////
 #include <dbstathelper.h>
+#include <qdbindivprovider.h>
+#include <qindivcluster.h>
+#include <qindivtreeelem.h>
+//////////////////////////
 #include "infotestdata.h"
 ///
 ///
@@ -14,6 +19,7 @@ class DTestStatDataTest : public QObject
     Q_OBJECT
 private:
     DBStatHelper *m_pHelper;
+    DBStatDataset m_oset;
     //
     static void import_dataset(DBStatHelper *pMan,
                                QString &name, int nRows, int nCols,
@@ -26,10 +32,15 @@ public:
 private Q_SLOTS:
     void initTestCase();
     void cleanupTestCase();
+    void init(void);
+    void cleanup();
+    //
     void testCaseDataSet();
     void testCaseVariables();
     void testCaseIndivs();
     void testCaseValues();
+    void testDBIndivProvider();
+    void testIndivCluster();
 };
 void DTestStatDataTest::import_dataset(DBStatHelper *pMan,
                                        QString &name, int nRows, int nCols,
@@ -142,6 +153,22 @@ void DTestStatDataTest::cleanupTestCase()
 {
     delete this->m_pHelper;
     this->m_pHelper = nullptr;
+}
+void DTestStatDataTest::init(void){
+    DBStatHelper *pMan = this->m_pHelper;
+    QVERIFY(pMan != nullptr);
+    //
+    QString name;
+    InfoTestData::get_mortal_name(name);
+    DBStatDataset oSet(name);
+    bool bRet = pMan->find_dataset(oSet);
+    QVERIFY(bRet);
+    QVERIFY(oSet.id() != 0);
+    this->m_oset = oSet;
+}
+
+void DTestStatDataTest::cleanup(){
+
 }
 
 void DTestStatDataTest::testCaseDataSet()
@@ -272,6 +299,116 @@ void DTestStatDataTest::testCaseValues(){
         QVERIFY(xVals.size() > 0);
     }// oVar
 }//testCaseValues
+void DTestStatDataTest::testDBIndivProvider(){
+    DBStatHelper *pMan = this->m_pHelper;
+    QVERIFY(pMan != nullptr);
+    DBStatDataset oSet(this->m_oset);
+    QVERIFY(oSet.id() != 0);
+    //
+    QDBIndivProvider oProvider(pMan,oSet,this);
+    IIndivProvider *pProvider = &oProvider;
+    QVERIFY(pProvider->is_valid());
+    int nCount = 0;
+    bool bRet = pProvider->indivs_count(nCount);
+    QVERIFY(bRet);
+    QVERIFY(nCount > 0);
+    QVector<IntType> oIds;
+    bRet = pProvider->all_indivs_ids(oIds);
+    QVERIFY(bRet);
+    QCOMPARE(oIds.size(),nCount);
+    int pos = 0;
+    Q_FOREACH(IntType aIndex, oIds)
+    {
+        bRet = pProvider->contains_id(aIndex);
+        QVERIFY(bRet);
+        QIndiv oInd1;
+        bRet = pProvider->find_indiv(aIndex,oInd1);
+        QVERIFY(bRet);
+        QCOMPARE(oInd1.id(),aIndex);
+        QIndiv oInd2;
+        bRet = pProvider->find_indiv_at(pos,oInd2);
+        QVERIFY(bRet);
+        QCOMPARE(oInd2.id(),aIndex);
+        if (pos > 0){
+            IntType aIndex1 = oIds[pos - 1];
+            IntType aIndex2 = oIds[pos];
+            double d1 = 0, d2 = 0;
+            bRet = pProvider->distance(aIndex1,aIndex2,d1);
+            QVERIFY(bRet);
+            QVERIFY(d1 > 0);
+            bRet = pProvider->distance_at(pos-1,pos,d2);
+            QVERIFY(bRet);
+            QVERIFY(d2 > 0);
+            QCOMPARE(d2,d1);
+        }// pos
+        ++pos;
+    }// aIndex
+}//estDBIndivProvider
+void DTestStatDataTest::testIndivCluster(){
+    DBStatHelper *pMan = this->m_pHelper;
+    QVERIFY(pMan != nullptr);
+    DBStatDataset oSet(this->m_oset);
+    QVERIFY(oSet.id() != 0);
+    QDBIndivProvider oProvider(pMan,oSet,this);
+    IIndivProvider *pProvider = &oProvider;
+    QVERIFY(pProvider->is_valid());
+    int nCount = 0;
+    bool bRet = pProvider->indivs_count(nCount);
+    QVERIFY(bRet);
+    QVERIFY(nCount > 0);
+    //
+    QIndivCluster oCluster1(1,pProvider,this);
+    QIndivCluster oCluster2(2,pProvider,this);
+    for (int i = 0; i < nCount; ++i){
+        QIndiv oInd;
+        bRet = pProvider->find_indiv_at(i,oInd);
+        QVERIFY(bRet);
+        IntType aIndex = oInd.id();
+        QVERIFY(aIndex != 0);
+        if ((i%2) == 0){
+            oCluster1.add_member(aIndex);
+        } else {
+            oCluster2.add_member(aIndex);
+        }
+    }//i
+    oCluster1.update_center();
+    oCluster2.update_center();
+    double dx = oCluster1.distance(&oCluster2);
+    QVERIFY(dx > 0);
+   // qDebug() << QString("Clusters distance ") << dx;
+    oCluster1.clear_members();
+    oCluster2.clear_members();
+    QMap<IntType,QVariant> c1,c2;
+    oCluster1.get_center(c1);
+    oCluster2.get_center(c2);
+   // qDebug() << "Center 1" << c1;
+   // qDebug() << "Center 2" << c2;
+    for (int i = 0; i < nCount; ++i){
+        QIndiv oInd;
+        bRet = pProvider->find_indiv_at(i,oInd);
+        QVERIFY(bRet);
+        IntType aIndex = oInd.id();
+        QVERIFY(aIndex != 0);
+        double d1 = oCluster1.distance(oInd);
+        double d2 = oCluster2.distance(oInd);
+        if (d1 < d2){
+            oCluster1.add_member(aIndex);
+        } else {
+            oCluster2.add_member(aIndex);
+        }
+    }//i
+    QVector<IntType> m1;
+    oCluster1.get_members(m1);
+   // qDebug() << QString("oCluster1 ") << m1;
+    QVector<IntType> m2;
+    oCluster2.get_members(m2);
+  //  qDebug() << QString("oCluster2 ") << m2;
+    oCluster1.get_center(c1);
+    oCluster2.get_center(c2);
+   // qDebug() << "Center 1" << c1;
+   // qDebug() << "Center 2" << c2;
+}//testIndivCluster
+
 //
 QTEST_MAIN(DTestStatDataTest)
 
